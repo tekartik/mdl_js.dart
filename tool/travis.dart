1bdd580df4c89056bd964171b0ea01bd8e0704aa
alex <alex@tekartik.com>
2020-11-16 21:19:16 +0100

    build: run_ci

--- tool/travis.dart
+++ tool/travis.dart
@@ -1,9 +1,4 @@
 import 'package:dev_test/package.dart';
-import 'package:pub_semver/pub_semver.dart';
-
-Version parsePlatformVersion(String text) {
-  return Version.parse(text.split(' ').first);
-}
 
 Future main() async {
   await packageRunCi('.');
